9c5b634bb6dac22a87151f1bc820daa4b514642a
andrey.blednykh <blednykhap@gmail.com>
2023-01-27 15:16:41 +0500

    Add column reindex

--- decadePrediction/callAPIwithPandas.py
+++ decadePrediction/callAPIwithPandas.py
@@ -48,8 +48,24 @@ def getCityData(path):
             "wind.direction.min.scale_8": "wind_direction_min_scale_8", "wind.direction.avg.degree": "wind_direction_avg_degree",
             "wind.direction.avg.scale_8": "wind_direction_avg_scale_8"})
 
+        columnsTitles = ['city', 'cloudiness_percent', 'cloudiness_type', 'date_local', 'date_time_zone_offset',
+                         'date_unix', 'date_utc', 'description_full', 'gm', 'humidity_percent_avg', 'humidity_percent_max',
+                         'humidity_percent_min', 'kind', 'phenomenon', 'precipitation_amount', 'precipitation_intensity',
+                         'precipitation_type', 'pressure_h_pa_max', 'pressure_h_pa_min', 'pressure_in_hg_max',
+                         'pressure_in_hg_min', 'pressure_mm_hg_atm_max', 'pressure_mm_hg_atm_min', 'storm',
+                         'temperature_air_avg_c', 'temperature_air_avg_f', 'temperature_air_max_c', 'temperature_air_max_f',
+                         'temperature_air_min_c', 'temperature_air_min_f', 'temperature_comfort_max_c', 'temperature_comfort_max_f',
+                         'temperature_comfort_min_c', 'temperature_comfort_min_f', 'temperature_water_max_c',
+                         'temperature_water_max_f', 'temperature_water_min_c', 'temperature_water_min_f',
+                         'wind_direction_avd_scale_8', 'wind_direction_avg_degree', 'wind_direction_max_degree',
+                         'wind_direction_max_scale_8', 'wind_direction_min_degree', 'wind_direction_min_scale_8',
+                         'wind_speed_avg_km_h', 'wind_speed_avg_m_s', 'wind_speed_avg_mi_h', 'wind_speed_max_km_h',
+                         'wind_speed_max_m_s', 'wind_speed_max_mi_h', 'wind_speed_min_km_h', 'wind_speed_min_m_s',
+                         'wind_speed_min_mi_h']
+
+        reindexDF = renamedDF.reindex(columns=columnsTitles)
         #renamedDF.info()
-        return renamedDF
+        return reindexDF
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
     except Exception as err:
